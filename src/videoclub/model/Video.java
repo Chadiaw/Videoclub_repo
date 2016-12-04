feb5e382d97d3158f577f4fb87a1b21fbb2fb7ba
Mel <belm104@uqo.ca>
2016-12-04 13:54:24 -0500

    04/12:
     - correction Video.java

---
+++ src/videoclub/model/Video.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoclub.model;
+
+/**
+ *
+ * @author Melanie
+ */
+public class Video extends Article{
+    
+    String titre;
+    String genre;
+    boolean nouveaute;
+    String synopsis;
+    String type; /*Blue-Ra */
+    
+    
+    
+}
